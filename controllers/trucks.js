4f90f02fe946a7523e49d4c62355a423e42cfeb3
c0nstantin1990 <constantinov.constantin@gmail.com>
2022-12-12 19:23:20 -0500

    change2

--- controllers/trucks.js
+++ controllers/trucks.js
@@ -18,5 +18,19 @@ router.get("/", async (req, res) => {
     res.status(400).json(error);
   }
 });
+router.get("/:id", async (req, res) => {
+  try {
+    res.json(await Truck.findById(req.params.id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+router.delete("/:id", async (req, res) => {
+  try {
+    res.json(await Truck.findOneAndRemove(req.params.id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 module.exports = router;
--- db/cars.js
+++ db/cars.js
@@ -1,9 +1,6 @@
-// const Car = require('../models/Car')
-// const cars = require('./cars.json')
+const Car = require("../models/Car");
+const cars = require("./cars.json");
 
-// Car.deleteMany({})
-//     .then(() => {
-//         Car.insertMany(cars)
-//             .then(console.log)
-//             .catch(console.error)
-//     })
+Car.deleteMany({}).then(() => {
+  Car.insertMany(cars).then(console.log).catch(console.error);
+});
